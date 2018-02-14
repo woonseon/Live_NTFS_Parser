643a9a1b2db73d2102539214f0f4e08db876e33f
woonseon <dbdnstjs8072@naver.com>
2018-02-14 15:00:36 +0900

    made 1.0 version

--- download.py
+++ download.py
@@ -5,7 +5,7 @@ from flask import send_from_directory
 
 def download(path, fname):
     try:
-        filePath = path + "/" + fname
-        return send_file(filePath, as_attachment=True)
+        return send_from_directory(path, fname)
+
     except Exception as e:
         error = "Invalid File Name"
